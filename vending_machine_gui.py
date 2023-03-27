51093c4c27412699af0559dc9be1931712f7cdb6
rakshithcp18 <120033311+rakshithcp18@users.noreply.github.com>
2023-03-27 16:50:34 +0530

    Update vending_machine_gui.py

--- vending_machine_gui.py
+++ vending_machine_gui.py
@@ -1,181 +1,196 @@
+import kivy
+import time
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.image import Image
+
+
+class CustomButton(Button):
+    snack_name = ""
+
+
+class CustomTextInput(TextInput):
+    pass
+
+
 class VendingMachine:
- 
-    total_revenue = 0               # Total revenue of all vending machines in the system
- 
-    snack_prices = {"candy": 2.00, "soda": 1.50, "chips": 3.00, "cookies": 3.50}   # dictionary with {<snack>: <amount>} as key-value pairs. Possible snacks: candy, soda, chips, cookies. Keys written in lowercase.
- 
-    # Instance attributes
-    def __init__(self, inventory, serial, days_until_maintenance):           # Initially, when an instance of the vending machine is created, the revenue is 0 and it's updated with each sale.
-        self.inventory = inventory        
-        self.revenue = 0                  
-        self.serial = serial
+    total_revenue = 0  
+
+    snack_prices = {"candy": 2.00, "soda": 1.50, "chips": 3.00, "cookies": 3.50}
+    snack_images = {"candy":"candy.jpg", "soda":"soda.jpg", "chips":"chips.jpg", "cookies":"cookies.jpg"}
+   
+    def __init__(self, inventory, serial, days_until_maintenance):
+        self.inventory = inventory  
+        self.revenue = 0  
         self.days_until_maintenance = days_until_maintenance
- 
-    # Method that displays an interactive menu to process a sale.
-    # Displays the options, gets user input to select the snack, and calls
-    # another method to process the sale.
-    def sales_menu(self):
- 
-        # The user has the option to buy several types of snacks
-        # so the program is repeated if the user indicates that he/she
-        # would like to buy another snack
-        while True:
- 
-            greetings = "\nWelcome! I have:\n"
-            request = "\nPlease enter the number of the item: "
- 
-            # Print a welcome message with the snacks available
-            print(greetings)
- 
-            i = 1
-            for snack in self.inventory:
-                print("(" + str(i) + ") " + snack.capitalize())
-                i += 1
- 
-            # Get the user input (option selected)
-            cust_input = int(input(request))
- 
-            # Repeat if the input doesn't meet the requireme1nts
-            while cust_input <=0 or cust_input >len(self.inventory):
-                print("Please enter a number from 1 to", len(self.inventory))
-                # Get the user input (option selected)
-                cust_input = int(input(request))
-        
-            # Display appropriate message
-            self.process_sale(list(self.inventory.keys())[cust_input - 1].lower())
-            answer = int(input("\nWould you like to buy another snack?\nEnter 1 for YES and 0 for NO: "))
- 
-            # If the customer does not wish to buy another snack
-            if not answer:
-                break
- 
- 
-    # Method that processes the sale by asking the user how many snacks of that type
-    # he/she would like to buy and calls another method to opdate the inventory
-    def process_sale(self, option): # option must be in lowercase
-        
-        print("\nYou selected: %s" % option.capitalize())
-        
-        if self.inventory[option] > 0:
+        self.purchase_made=False
+
+    
+    def sales_menu(self, instance):
+        content = BoxLayout(orientation='vertical')
+        greetings = Label(text="Welcome! I have:")
+        content.add_widget(greetings)
+        for snack, quantity in self.inventory.items():
+            inventory_label = Label(text=f"{snack.capitalize()} :  Available Quantity {quantity}")
+            content.add_widget(inventory_label)
+
+             # Create an Image widget for the snack
+            snack_image = Image(source=self.snack_images[snack])
+            content.add_widget(snack_image)
             
-            # Display current snack inventory and product
-            print("Great! I currently have %d %s in my inventory\n" % (self.inventory[option], option))
-            
-            # Ask for the number of snacks
-            num_items = int(input("How many %s would you like to buy?\n" % option))
- 
-            # Handle cases where user might enter a negative number or zero
-            while num_items <= 0:
-                print("Please enter a positive integer")
-                num_items = int(input("\nHow many %s would you like to buy?\n" % option))
- 
-            # Update inventory if there are enough snacks available
-            if num_items <= self.inventory[option]:
-                self.remove_from_inventory(option, num_items)
-                
-                # Update the machine's revenue
-                total = self.update_revenue(option, num_items)
- 
-                print("That would be: $ " + str(total))
- 
-                # Display a message confirming the purchase and current inventory
-                print("\nThank you for your purchase!")
-                print("Now I have %d %s and my revenue is $%d" % (self.inventory[option], option, self.revenue))
+            button = CustomButton(text="Buy")
+            button.snack_name = snack
+            button.bind(on_press=self.process_sale)
+            content.add_widget(button)
+
+        if self.purchase_made:
+            back_button = Button(text='Back to Main Menu')
+            back_button.bind(on_press=self.back_to_menu)
+            content.add_widget(back_button)
 
 
-                # Remove item from menu if inventory is depleted
-                if self.inventory[option] == 0:
-                    del self.inventory[option]
+        popup = Popup(title='Select a snack', content=content, size_hint=(0.5, 0.5))
+        popup.open()
+
+    def back_to_menu(self, instance):
+        self.purchase_made = False  # reset the flag when going back to the main menu
+        self.sales_menu(instance)
+
+    
+    def process_sale(self, button):
+        option = button.snack_name
+        print(f"Button {option.capitalize()} was pressed.")
+        content = BoxLayout(orientation='vertical')
+        prompt = Label(text=f"How many {option.capitalize()} would you like to buy?")
+        input_box = TextInput(multiline=False)
+        content.add_widget(prompt)
+        content.add_widget(input_box)
+    
+    
+        def update_inventory(self, snack_name, quantity):
+            if snack_name in self.inventory:
+                self.inventory[snack_name] += quantity
+            else:                
+                self.inventory[snack_name] = quantity
+
+
+        def buy_snacks(button):
+            num_items_str = input_box.text
+            try:
+                num_items = int(num_items_str)
+            except ValueError:
+                error_msg = Label(text="Please enter a positive integer")
+                content.add_widget(error_msg)
+                return
+
+            if num_items <= 0:
+                error_msg = Label(text="Please enter a positive integer")
+                content.add_widget(error_msg)
+                return
                 
-            else:
-                print("I don't have so many %s. Sorry! :(" % option)
                 
+            if num_items > self.inventory[option]:
+                error_msg = Label(text="I'm sorry, I don't have enough {} in stock. Please choose a smaller quantity".format(option))
+                content.add_widget(error_msg)
+                return
+
+    
+            total_price = num_items * self.snack_prices[option]
+            self.revenue += total_price
+            VendingMachine.total_revenue += total_price
+
+
+            self.inventory[option] -= num_items
+            confirmation_msg = Label(text="Great! That will be ${:.2f}. Enjoy your {}!".format(total_price, option))
+            content.add_widget(confirmation_msg)
+            time.sleep(0)
+
+            # Show a popup asking if the user wants to make another purchase
+            another_purchase_popup_content = BoxLayout(orientation='vertical')
+            another_purchase_prompt = Label(text="Would you like to buy the  another item?")
+            yes_button = Button(text='Yes')
+            no_button = Button(text='No')
+
+
+            def show_sales_menu(button):
+                self.sales_menu(None)
+
+
+            def dismiss_popup(button):
+                popup.dismiss()
+
+
+            yes_button.bind(on_press=show_sales_menu)
+            no_button.bind(on_press=dismiss_popup)
+
+
+            another_purchase_popup_content.add_widget(another_purchase_prompt)
+            another_purchase_popup_content.add_widget(yes_button)
+            another_purchase_popup_content.add_widget(no_button)
+
+
+            popup = Popup(title='Purchase Complete', content=another_purchase_popup_content, size_hint=(0.5, 0.5))
+            popup.open()
+            
+
+        buy_button = Button(text='Buy')
+        buy_button.bind(on_press=buy_snacks)
+        content.add_widget(buy_button)
+        
+
+        popup= Popup(title=option.capitalize(), content=content, size_hint=(0.5, 0.5))
+        popup.open()
+
+
+    def check_maintenance(self):
+        if self.days_until_maintenance == 0:
+            msg = "Maintenance is required for vending machine {}.".format(self.serial)
+            content = BoxLayout(orientation='vertical')
+            content.add_widget(Label(text=msg))
+            popup = Popup(title="Maintenance Required", content=content, size_hint=(0.5, 0.5))
+            popup.open()
+            
         else:
-            print("I don't have any more %s. Sorry! :(" % option)
- 
- 
-    # Method that updates the vending machine's (instance) inventory by
-    # decrementing the availability of the snack chosen.
-    def remove_from_inventory(self, option, num_items):
-        self.inventory[option] -= num_items
+            msg = "Vending machine {} is in good condition.".format(self.serial)
+            content = BoxLayout(orientation='vertical')
+            content.add_widget(Label(text=msg))
+            popup = Popup(title="Maintenance Check", content=content, size_hint=(0.5, 0.5))
+            popup.open()
 
 
-    # Update the revenue of the instance of VendingMachine
-    # and update the class attribute total revenue.
-    def update_revenue(self, option, num_items):
-        # Find price of the snack
-        price = self.find_snack_price(option)
- 
-        # Update Instance and class
-        self.revenue += num_items * price
-        VendingMachine.total_revenue += num_items * price
- 
-        return num_items * price
- 
-    # Find the price of the snack selected in the class attribute
-    # and return it
-    def find_snack_price(self, snack):
-        return VendingMachine.snack_prices[snack]        
+class VendingMachineWidget(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        #size: root.width;root.height
+        self.vm = VendingMachine(inventory={"candy": 100, "soda": 100, "chips": 100, "cookies": 100}, serial="VM001", days_until_maintenance=5)
+        layout = BoxLayout(orientation='vertical')
+        sales_button = Button(text='Sales Menu',font_size=15,size_hint=(None,None), size=(200,100),pos=(Window.width, Window.height))
+        sales_button.bind(on_press=self.vm.sales_menu)
+        layout.add_widget(sales_button)
+        self.add_widget(layout)
         
-    # Method that prints a message with the total revenue of the instance of VendingMachine
-    def display_revenue(self):
-        print("The total revenue of this vending machine is:", self.revenue)
-                      
- 
-# Subclasses
- 
-class HospitalVendingMachine(VendingMachine):
 
-    snack_prices = {"candy": 1.00, "soda": 15.00, "chips": 30.0, "cookies": 35.0}
-
-    def __init__(self, inventory, serial, days_until_maintenance):
-        VendingMachine.__init__(self, inventory, serial, days_until_maintenance)
-
-    def sales_menu(self):
-        print("Welcome to our Hospital Vending Machine\nWe hope you are feeling better today!")
-        VendingMachine.sales_menu(self)
-
-    def find_snack_price(self, snack):
-        return HospitalVendingMachine.snack_prices[snack]
-    
-    def print_days_until_maintenance(self):
-        print("i have %d until my next days of the maintance " % self.days_until_maintenance)
+class VendingMachineApp(App):
+    def build(self):
+        root_widget = BoxLayout(orientation='vertical')
+        vm_widget = VendingMachineWidget()
+        root_widget.add_widget(vm_widget)
+        return root_widget
 
 
-
-class SchoolVendingMachine(VendingMachine):
-
-    snack_prices = {"candy": 0.50, "soda": 1.00, "chips": 3.0, "cookies": 3.50}
-
-    student_debt ={"abc":50 , "def":60}
+    def on_start(self):
+        pass
 
 
-    def __init__(self, inventory, serial, days_until_maintenance):
-        VendingMachine.__init__(self, inventory, serial, days_until_maintenance)
-
-    
-    def sales_menu(self):
-        print("Welcome to our School Vending Machine\n We hope you have a great day full of learning!")
-        VendingMachine.sales_menu(self)
+if __name__ == '__main__':
+    VendingMachineApp().run()
 
 
-    def find_snack_price(self, snack):
-        return SchoolVendingMachine.snack_prices[snack]
-    
-
-    def print_student_debt(self,student):
-        print(" the student %s has having reaming debt of $%d " % (student,SchoolVendingMachine.student_debt))
-    
- 
- 
-# Instances
- 
-floor_machine = VendingMachine({"candy": 36, "soda": 15, "chips": 40, "cookies": 120}, "011423424", 24)
-floor_machine.sales_menu()
- 
-# hospital_machine = HospitalVendingMachine({"candy": 32, "soda": 50, "chips": 45, "cookies": 80}, "03223424", 15)
-# hospital_machine.sales_menu()
- 
-# school_machine = SchoolVendingMachine({"candy": 36, "soda": 15, "chips": 40, "cookies": 120}, "0534424", 12) 
-# school_machine.sales_menu()
 
